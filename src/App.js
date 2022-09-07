e6c9c8ccf4cf245dfd56a39fd707a4a9b7572a93
Khoa Nguyễn <khoaruoi69@gmail.com>
2022-09-07 16:48:11 +0700

    useState

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+
+const Gifts =[ 
+  'CPU 9',
+  'Ram 32 Gb',
+  'RGB keyboard'
+]
+function App() {
+
+
+
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
