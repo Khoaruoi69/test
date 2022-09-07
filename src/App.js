5fde7673a0867756d506b6a82ed38fe636c43b9d
Khoa Nguyễn <khoaruoi69@gmail.com>
2022-09-07 16:56:48 +0700

    gifts

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ const Gifts =[
   'RGB keyboard'
 ]
 function App() {
+  const [gift, setGift] =useState()
 
-
-
+ const randomGift =()=>{
+  const index = Math.floor(Math.random()*Gifts.length);
+  setGift(Gifts[index]);
+ }
 
   return (
-    <div className="App">
-      
+    <div style={{padding: '10px'}}>
+      <h1>{gift||"Bấm nhận thưởng nào"}</h1>
+      <button onClick={randomGift}> Nhận thưởng</button>
     </div>
   );
 }
