3f5c0ac9bffdd314cb8941a7555b91a1bf40e3f3
Khoa Nguyễn <khoaruoi69@gmail.com>
2022-09-09 15:10:51 +0700

    with caht app

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-
-
-
-// fake comment 
-function emitComment(id){
-  setInterval(()=>{
+// fake comment
+function emitComment(id) {
+  setInterval(() => {
     // tự động phát một event đối tượng window
     window.dispatchEvent(
-      new CustomEvent(`lesson-${id}`,{
-    detail: `Nội dung comment của lesson ${id}`
-  })
-    )
-  }, 2000)
+      new CustomEvent(`lesson-${id}`, {
+        detail: `Nội dung comment của lesson ${id}`,
+      })
+    );
+  }, 2000);
 }
-emitComment(1)
-emitComment(2)
-emitComment(3)
+emitComment(1);
+emitComment(2);
+emitComment(3);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
