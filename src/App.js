f66281681f885f5a3e214aeef377b457e5a1a738
Khoa Nguyễn <khoaruoi69@gmail.com>
2022-09-11 19:04:23 +0700

    dùng useMemo() đẻ tính mảng ko lặp

--- src/App.js
+++ src/App.js
@@ -3,18 +3,10 @@ import Content from "./Content";
 
 
 function App() {
-  const [count2, setCount2]= useState(10)
-
-
-  const handleCount2 = useCallback(()=>{
-    setCount2(pre=>pre+1);
-  },[])
 
   return (
     <div style={{ padding: 32}}>
-     <Content onIncrease2={handleCount2}/>
-     <h1>{count2 }</h1>
-     
+      <Content/>
     </div>
   )
 }
--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,4 @@
-import { useEffect, useInsertionEffect, useState, useRef, memo } from "react";
+import { useEffect, useInsertionEffect, useState, useRef, memo, useMemo } from "react";
 // memo chỉ chạy khi có một thằng components thay đổi, nếu ko có thay đổi gì thì nó giữ nguyên ko thay đổi
 /**
  * 1. memo() - > higher Order  Components(HOC)
@@ -12,13 +12,56 @@ import { useEffect, useInsertionEffect, useState, useRef, memo } from "react";
 
 function Content({count, onIncrease2}) {
 
-  console.log("re-render");
+  const [name,setName] = useState()
+  const [price,setPrice] = useState()
+  const [products,setProducts] = useState([])
 
+  // useRef để đưa con trỏ chuột về ô input
+   const nameRef = useRef()
+
+  const handleSubmit=()=>{
+    setProducts([...products,{
+      name: name,
+      price: +price
+    }])
+    setName('')
+    setPrice('')
+    nameRef.current.focus()
+  }
+
+  // When the product changes, the use changes.
+  const total = useMemo(()=>{
+    const results = products.reduce((results, product)=>{
+      console.log('recalculation')
+      return results + product.price;
+    },0)
+    return results;
+  },[products])
   return (
-    <div>
-    <h1>heloo o o oosd </h1>
-     <button onClick={ onIncrease2}>Click me 2!</button>
+    <div style={{padding:'10px 32px'}}>
+      <input 
+        ref={nameRef}
+        value={name}
+        placeholder="Enter name..."
+        onChange={e=>setName(e.target.value)}
+      />
+      <br/>
+      <input 
+        value={price}
+        placeholder="Enter price..."
+        onChange={e=>setPrice(e.target.value)}
+      />
+      <br/>
+      <button onClick={handleSubmit}> Add</button>
+      <br/>
+      Total:{total}
+      <ul>
+        {products.map((product,index)=>(
+          <li key={index}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
-export default memo(Content);
+export default memo(Content)
